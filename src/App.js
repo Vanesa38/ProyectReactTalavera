572f04414db3dc21eb07a5cddde5737ef2e755f4
vane_talavera <vanetala32@gmail.com>
2022-09-25 16:34:13 -0300

    Proyectreacttalavera

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <ItemListContainer greeting={"Bienvenidos"}/>
     </div>
   );
 }
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,12 +1,13 @@
-import cart from "./assets/carrito-de-compras (1).png"
+import cart from "./assets/Carro1.PNG";
+import  "../CartWidget/CartWidget.css"
 
 const CartWidget = () => {
    return (
     <div>
         <img src={cart} alt="cart" />
-        
+        <span id="contador">0</span>
     </div>
-   )
+   );
 }
 
 export default CartWidget
